ee751c50a75ac05d6bd3623822e4f3809cd38df5
Aji Setiawan <Masaji11223@gmail.com>
2023-04-03 05:29:54 +0700

    dart library

---
+++ example/hello.dart
@@ -0,0 +1,10 @@
+import 'package:belajar_dart_library/hello.dart';
+import 'package:belajar_dart_library/src/customer.dart';
+
+void main() {
+  print(sayHello('Aji'));
+
+  var customer = Customer();
+  var product = Product();
+  var category = Categoty();
+}
